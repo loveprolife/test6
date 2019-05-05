13eeba68b874f90c6a574ccabb8e8fca728be91b
王鹏 <peng.wang@wenba100.com>
2019-05-05 17:10:05 +0800

    sadf

--- 002/dao/dao_chuzhong_kexue_002.php
+++ 002/dao/dao_chuzhong_kexue_002.php
@@ -3,7 +3,7 @@ ini_set('memory_limit','1024M');
 require_once  "PHPExcel-1.8/Classes/PHPExcel/IOFactory.php";
 
 //需要读取的execl文件
-$filename = '001.xlsx';
+$filename = './xiaotu/chuzhong_kexue/七下期中/002.xlsx';
 
 //加载excel文件
 $excel = \PHPExcel_IOFactory::load($filename);
@@ -15,12 +15,27 @@ foreach ($excel->getWorksheetIterator() as $sheet){
         $tmp = array();
         $tag = 0;
         foreach ($row->getCellIterator() as $cell){
-            if($tag == 205){
+//            print($tag . "||" . $cell->getValue() . "\r\n");
+            if($tag == 1){
+                $data = $cell->getValue();
+            }elseif($tag == 3){
+                $data = $cell->getValue();
+            }elseif($tag == 5){
+                $data = $cell->getValue();
+            }elseif($tag == 7){
+                $data = $cell->getValue();
+            }elseif($tag == 68){
+                $data = $cell->getCalculatedValue();
+            }elseif($tag == 69){
+                $data = $cell->getCalculatedValue();
+            }elseif($tag == 70){
                 $data = $cell->getCalculatedValue();
             }else{
                 $data = $cell->getValue();
             }
-            array_push($tmp, $data);
+            if($tag == 1 || $tag == 3 || $tag == 5 || $tag == 7 || $tag == 68 || $tag == 69 || $tag == 70){
+                array_push($tmp, $data);
+            }
             ++$tag;
         }
         array_push($ttt, $tmp);
@@ -29,11 +44,10 @@ foreach ($excel->getWorksheetIterator() as $sheet){
 
 $connect = mysqli_connect('10.2.1.13','wenba','szc0219','oneone_recommend','3306');
 mysqli_query($connect,'set names utf8');
-mysqli_query($connect, "truncate test001");
+mysqli_query($connect, "truncate chuzhong_kexue_002");
 for($i = 0; $i < count($ttt); $i++){
-    $sql = 'INSERT INTO test001 SET yi_name = "'.$ttt[$i][3].'", er_name = "'.$ttt[$i][5].'", san_name = "'.$ttt[$i][7].'", si_name = "'.$ttt[$i][9].'", zhi = "'.$ttt[$i][205].'"';
+    $sql = "INSERT INTO chuzhong_kexue_002 SET yi_name = '".$ttt[$i][0]."', er_name = '".$ttt[$i][1]."', san_name = '".$ttt[$i][2]."', si_name = '".$ttt[$i][3]."', zhi_hangzhou = '".$ttt[$i][4]."', zhi_wenzhou = '".$ttt[$i][6]."', zhi_jinhua = '".$ttt[$i][5]."', zhi_ningbo = '0'";
+//    print_r($sql . "\r\n");
     $result = mysqli_query($connect,$sql);
 }
 mysqli_close($connect);
-
-?>
