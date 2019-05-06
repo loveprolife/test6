4575ac693a18cb01f8320b435feb73a2a8c232db
王鹏 <peng.wang@wenba100.com>
2019-05-06 18:06:45 +0800

    asdf

---
+++ 002/pdf2png.py
@@ -0,0 +1,22 @@
+from pdf2image import convert_from_path
+from PIL import Image
+
+Image.MAX_IMAGE_PIXELS = None
+
+images = convert_from_path('/Users/wangpeng/Downloads/数据可视化/002/chuzhong_kexue.pdf', dpi=900)
+images[0].save('/Users/wangpeng/Downloads/数据可视化/002/chuzhong_kexue.png')
+
+images = convert_from_path('/Users/wangpeng/Downloads/数据可视化/002/chuzhong_yingyu.pdf', dpi=900)
+images[0].save('/Users/wangpeng/Downloads/数据可视化/002/chuzhong_yingyu.png')
+
+images = convert_from_path('/Users/wangpeng/Downloads/数据可视化/002/chuzhong_yuwen.pdf', dpi=900)
+images[0].save('/Users/wangpeng/Downloads/数据可视化/002/chuzhong_yuwen.png')
+
+images = convert_from_path('/Users/wangpeng/Downloads/数据可视化/002/gaozhong_shuxue.pdf', dpi=900)
+images[0].save('/Users/wangpeng/Downloads/数据可视化/002/gaozhong_shuxue.png')
+
+images = convert_from_path('/Users/wangpeng/Downloads/数据可视化/002/gaozhong_yingyu.pdf', dpi=900)
+images[0].save('/Users/wangpeng/Downloads/数据可视化/002/gaozhong_yingyu.png')
+
+images = convert_from_path('/Users/wangpeng/Downloads/数据可视化/002/gaozhong_yuwen.pdf', dpi=900)
+images[0].save('/Users/wangpeng/Downloads/数据可视化/002/gaozhong_yuwen.png')
