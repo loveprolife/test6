da137bf7e30d9dc8eec0cfdb4e10de664da25f7b
王鹏 <peng.wang@wenba100.com>
2019-05-06 11:19:00 +0800

    asdf

---
+++ 002/chuzhong_yuwen/chuzhong_yuwen_002_ningbo.php
@@ -0,0 +1,24 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+$connect = mysqli_connect('10.2.1.13','wenba','szc0219','oneone_recommend','3306');
+mysqli_query($connect,'set names utf8');
+
+$sql_san = 'SELECT id, san_name, SUM(zhi_ningbo) FROM chuzhong_yuwen_002 GROUP BY san_name ORDER BY id ASC';
+$result_san = mysqli_query($connect, $sql_san)->fetch_all();
+
+$connect->close();
+
+$rrrr = array();
+for($i = 0; $i < count($result_san); $i++){
+//    if((float)$result_san[$i][2] > 0){
+    $t = array();
+    $t['name'] = $result_san[$i][1];
+    $t['value'] = $result_san[$i][2];
+    array_push($rrrr, $t);
+//    }
+}
+
+
+print_r(json_encode($rrrr));
